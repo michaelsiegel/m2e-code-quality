2437a2672b8a01e97a29864d9f1ef950984cf6f8
Michael Siegel <ms@i2dm.de>
2012-06-30 17:44:45 +0200

    Update Juno
    Signed-off-by: Michael Siegel <siegel@uni-bremen.de>

--- com.basistech.m2e.code.quality.shared/src/main/java/com/basistech/m2e/code/quality/shared/MavenPluginWrapper.java
+++ com.basistech.m2e.code.quality.shared/src/main/java/com/basistech/m2e/code/quality/shared/MavenPluginWrapper.java
@@ -29,113 +29,108 @@ import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 
-
 public class MavenPluginWrapper {
-	private String key; // for toString
-	private final MojoExecution execution;
-    
-    private MavenPluginWrapper(final String key, final MojoExecution execution) {
-    	this.key = key;
-    	this.execution = execution;
-    }
+  public static MojoExecution findMojoExecution(IProgressMonitor monitor,
+      IMavenProjectFacade mavenProjectFacade, final String pluginGroupId,
+      final String pluginArtifactId, final String[] pluginGoal) throws CoreException {
+    List<MojoExecution> mojoExecutions = mavenProjectFacade.getMojoExecutions(pluginGroupId,
+        pluginArtifactId, monitor, pluginGoal);
+    // I don't think we need to re-search for site.
+    return searchExecutions(pluginGroupId, pluginArtifactId, pluginGoal, mojoExecutions);
+  }//
 
-    public boolean isPluginConfigured() {
-        return this.execution == null ? false : true;
-    }
-    
-    public MojoExecution getMojoExecution() {
-    	return execution;
-    }
-    
+  public static boolean mojoExecutionForPlugin(MojoExecution mojoExecution, String groupId,
+      String artifactId, String goal) {
+    return groupId.equals(mojoExecution.getGroupId())
+        && artifactId.equals(mojoExecution.getArtifactId())
+        && (goal == null || goal.equals(mojoExecution.getGoal()));
+  }
 
-    /**
-     * Return a list of dependencies of the plugin as configured in the
-     * {@code <dependencies>} element of the plugin configuration <em>including</em>
-     * the plugin itself, which will the always be the first element of the
-     * returned list of dependencies.
-     * 
-     * <p>
-     * This is mainly to build an isolated classpath for the plugin.
-     * </p>
-     * @return a {@code List} of the plugin dependencies, if the plugin was actually
-     *         configured in the project interpolated {@code <build>} elements, else
-     *         returns an {@code empty list}.
-     */
-    public List<Dependency> getDependenciesIncludingSelf() {
-        if (this.execution == null) {
-            return Collections.emptyList();
+  public static MavenPluginWrapper newInstance(IProgressMonitor monitor,
+      final String pluginGroupId, final String pluginArtifactId, final String[] pluginGoal,
+      IMavenProjectFacade mavenProjectFacade) throws CoreException {
+    Preconditions.checkNotNull(mavenProjectFacade);
+    final MojoExecution execution = findMojoExecution(monitor, mavenProjectFacade, pluginGroupId,
+        pluginArtifactId, pluginGoal);
+    String key = pluginGroupId + ":" + pluginArtifactId;
+    return new MavenPluginWrapper(key, execution);
+  }
+
+  private static MojoExecution searchExecutions(final String pluginGroupId,
+      final String pluginArtifactId, final String[] pluginGoal, List<MojoExecution> mojoExecutions) {
+    for (MojoExecution mojoExecution : mojoExecutions) {
+      if (pluginGoal != null) {
+        for (String goal : pluginGoal) {
+          if (mojoExecutionForPlugin(mojoExecution, pluginGroupId, pluginArtifactId, goal)) {
+            return mojoExecution;
+          }
         }
-        List<Dependency> dl = new ArrayList<Dependency>();
-        dl.add(this.getPluginAsDependency());
-        dl.addAll(execution.getPlugin().getDependencies());
-        return ImmutableList.copyOf(dl);
+      } else {
+        if (mojoExecutionForPlugin(mojoExecution, pluginGroupId, pluginArtifactId, null)) {
+          return mojoExecution;
+        }
+      }
     }
+    return null;
+  }
 
-    private Dependency getPluginAsDependency() {
-        final Dependency d = new Dependency();
-        d.setGroupId(execution.getPlugin().getGroupId());
-        d.setArtifactId(execution.getPlugin().getArtifactId());
-        d.setVersion(execution.getPlugin().getVersion());
-        d.setType("maven-plugin");
-        d.setClassifier(null);
-        return d;
-    }
-    
-    public static boolean mojoExecutionForPlugin(MojoExecution mojoExecution, String groupId, String artifactId,
-    			String goal) {
-    	return groupId.equals(mojoExecution.getGroupId())
-    			&& artifactId.equals(mojoExecution.getArtifactId())
-    			&& (goal == null || goal.equals(mojoExecution.getGoal()));
-    }
-    
-    public static MojoExecution findMojoExecution(
-    		IProgressMonitor monitor,
-            IMavenProjectFacade mavenProjectFacade,
-            final String pluginGroupId,
-            final String pluginArtifactId,
-            final String[] pluginGoal) throws CoreException {
-        List<MojoExecution> mojoExecutions = mavenProjectFacade.getMojoExecutions(pluginGroupId, pluginArtifactId, monitor, pluginGoal);
-        // I don't think we need to re-search for site.
-        return searchExecutions(pluginGroupId, pluginArtifactId, pluginGoal, mojoExecutions);
-    }//
+  private final String key; // for toString
 
-	private static MojoExecution searchExecutions(final String pluginGroupId,
-			final String pluginArtifactId, final String[] pluginGoal,
-			List<MojoExecution> mojoExecutions) {
-		for (MojoExecution mojoExecution : mojoExecutions) {
-    		if (pluginGoal != null) {
-    			for (String goal : pluginGoal) {
-    	    		if(mojoExecutionForPlugin(mojoExecution, pluginGroupId, pluginArtifactId, goal)) {
-    	    			return mojoExecution;
-    	    		}
-    			}
-    		} else {
-    			if(mojoExecutionForPlugin(mojoExecution, pluginGroupId, pluginArtifactId, null)) {
-	    			return mojoExecution;
-	    		}
-    		}
-    	}
-		return null;
-	}
-    
-    public static MavenPluginWrapper newInstance(
-    		IProgressMonitor monitor,
-            final String pluginGroupId,
-            final String pluginArtifactId,
-            final String[] pluginGoal,
-            IMavenProjectFacade mavenProjectFacade) throws CoreException {
-        Preconditions.checkNotNull(mavenProjectFacade);
-        final MojoExecution execution = findMojoExecution(monitor, mavenProjectFacade, pluginGroupId, pluginArtifactId, pluginGoal);
-        String key = pluginGroupId + ":" + pluginArtifactId;
-        return new MavenPluginWrapper(key, execution);
+  private final MojoExecution execution;
+
+  private MavenPluginWrapper(final String key, final MojoExecution execution) {
+    this.key = key;
+    this.execution = execution;
+  }
+
+  /**
+   * Return a list of dependencies of the plugin as configured in the
+   * {@code <dependencies>} element of the plugin configuration
+   * <em>including</em> the plugin itself, which will the always be the first
+   * element of the returned list of dependencies.
+   * 
+   * <p>
+   * This is mainly to build an isolated classpath for the plugin.
+   * </p>
+   * 
+   * @return a {@code List} of the plugin dependencies, if the plugin was
+   *         actually configured in the project interpolated {@code <build>}
+   *         elements, else returns an {@code empty list}.
+   */
+  public List<Dependency> getDependenciesIncludingSelf() {
+    if (this.execution == null) {
+      return Collections.emptyList();
     }
-    
-    @Override
-	public String toString() {
-    	String s = "[MavenPluginWrapper " + key;
-    	if (execution == null) {
-    		s = s + " null wrapper]";
-    	}
-    	return s;
-	}
+    List<Dependency> dl = new ArrayList<Dependency>();
+    dl.add(this.getPluginAsDependency());
+    dl.addAll(this.execution.getPlugin().getDependencies());
+    return ImmutableList.copyOf(dl);
+  }
+
+  public MojoExecution getMojoExecution() {
+    return this.execution;
+  }
+
+  private Dependency getPluginAsDependency() {
+    final Dependency d = new Dependency();
+    d.setGroupId(this.execution.getPlugin().getGroupId());
+    d.setArtifactId(this.execution.getPlugin().getArtifactId());
+    d.setVersion(this.execution.getPlugin().getVersion());
+    d.setType("maven-plugin");
+    d.setClassifier(null);
+    return d;
+  }
+
+  public boolean isPluginConfigured() {
+    return this.execution == null ? false : true;
+  }
+
+  @Override
+  public String toString() {
+    String s = "[MavenPluginWrapper " + this.key;
+    if (this.execution == null) {
+      s = s + " null wrapper]";
+    }
+    return s;
+  }
 }
